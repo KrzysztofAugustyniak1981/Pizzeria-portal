d4ecc0f25786ff26e66f800001bad30166eebc36
Krzysztof Augustyniak <augustyniak@o2.pl>
2021-01-26 14:30:39 +0100

    add Nav links and view

---
+++ src/components/views/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Home.scss';
+
+function Home() {
+    return (
+        <div className={styles.component}>
+            <h2>Home view</h2>
+        </div>
+    );
+}
+
+export default Home;
